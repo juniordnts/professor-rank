789ac2bf4839022b331604b3d010a5fce717c4d1
Jackson <j.dantas27@gmail.com>
2019-10-12 17:46:28 -0300

    #05 Rank Principal Pronto

---
+++ front/assets/classes/Main.js
@@ -0,0 +1,23 @@
+
+
+var data = $.getJSON("/dados/2019-2017-refined.json")
+
+data.then((res) => {
+
+  // $("#table-geral").html("")
+
+  $.each(res, function (key, item) {
+    let record = new Professor(key, item["Componentes"], item["MediaDeNotas"], item["MediaDeDesvios"], item["DesvioDeNotas"])
+    $("#table-geral").append(record.template.geral)
+  });
+
+  setup_assets();
+
+  $("#buscar-professor").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#geral tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+})
