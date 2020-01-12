9b3ed7c5f1b2ae41f552d50d696f104a5cc240dc
Jackson <j.dantas27@gmail.com>
2020-01-12 12:24:10 -0300

    #10 Título, Add CE e CCHLA

--- front/assets/classes/Main.js
+++ front/assets/classes/Main.js
@@ -1,9 +1,14 @@
 
-var folder = window.location.search.split("=")[1]
+var folder = window.location.search.split("=")[1].split("&")[0]
+var depName = decodeURI(window.location.search.split("=")[2])
+
+depName != "undefined" ? $("#dep-name").text(depName) : NaN
+
 var data = $.getJSON("../dados/" + folder + "/2019-2017-refined.json")
 
 data.then((res) => {
 
+  $("#loader").remove()
   $.each(res, function (key, item) {
     let record = new Professor(key, item["Componentes"], item["MediaDeNotas"], item["MediaDeDesvios"], item["DesvioDeNotas"])
     $("#table-geral, #table-professores").append(record.template.geral)
