b2cb27a0752dbedcf31d44163ccdf103d54b5f9a
Jackson <j.dantas27@gmail.com>
2020-02-26 14:57:31 -0300

    Analytics

---
+++ front/assets/js/main.analytics.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBr9uzgiXIXVTThihN4JYt7mSZYMjSQQL0",
+  authDomain: "geral-dnts.firebaseapp.com",
+  databaseURL: "https://geral-dnts.firebaseio.com",
+  projectId: "geral-dnts",
+  storageBucket: "geral-dnts.appspot.com",
+  messagingSenderId: "351134168292",
+  appId: "1:351134168292:web:020ad821d02b8c7837a8f1",
+  measurementId: "G-4468L6TL58"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
